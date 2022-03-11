38644ba4a28c26bfc18582445958d20fb23806f5
ARodsIsland2 <arods.island.2@gmail.com>
2022-03-11 22:14:57 +0000

    Added Stripe response handler and error handling js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,7 +3,7 @@
 // Document ready
 $(document).on('turbolinks:load', function(){
   
-    var theForm = $('#pro_form');
+  var theForm = $('#pro_form');
   var submitBtn = $('#form-submit-btn');
   
   // Set stripe public key
@@ -15,28 +15,60 @@ $(document).on('turbolinks:load', function(){
     
     // prevent default submission behavior
     event.preventDefault();
+    submitBtn.val("Processing").prop('disabled', true);
     
     // Collect credit card fields
     var ccNum = $('#card_number').val(),
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-
-    // Send card info to stripe
-    Stripe.createToken({
-      number: ccNum,
-      cvc: cvcNum,
-      exp_month: expMonth,
-      exp_year: expYear
-    }, stripeResponseHandler);
+        
+    // Use stripe js library to check for card errors.
+    var error = false;
     
+    // Validate cad information.
+    if ( !Stripe.card.validateCardNumber(ccNum) ) {
+      error = true;
+      alert('The credit card number appears to be invalid');
+    }
+    if ( !Stripe.card.validateCVC(cvcNum) ) {
+      alert('The credit card cvc number appears to be invalid');
+      error = true;
+    }
+    if ( !Stripe.card.validateExpiry(expMonth, expYear) ) {
+      alert('The expiration date appears to be invalid');
+      error = true;
+    }
     
+    if (error) {
+      // If there are card errors, don't send to stripe
+      submitBtn.val("Sign Up").prop('disabled', false);
+    } else
+    {
+      // Send card info to stripe
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    
+    return false;
   });
   
+  // Stripe will return a card token.
+  function stripeResponseHandler(status, response) {
+    
+    // Get the token from the response
+    var token = response.id;
+    
+    // Inject card token as hidden field into form
+    theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+    
+    // Submit form to our Rails app  
+    theForm.get(0).submit();
+  }
   
-  // Inject card token as hidden field into form.
   
-  // Submit form to our Rails app
-
-
 });
