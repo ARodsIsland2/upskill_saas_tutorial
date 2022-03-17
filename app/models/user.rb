3e4e53aafa3e4ba6a36e99253263c41d7f2609c0
ARodsIsland2 <arods.island.2@gmail.com>
2022-03-17 21:00:29 +0000

    Replaced pro plan stripe id with their new price

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,8 @@ class User < ApplicationRecord
   # Store customer.id as the stripe_customer_token and save the user.
   def save_with_subscription
     if valid?
-      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      #customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      customer = Stripe::Customer.create(description: email, plan: 'price_1KZhcMIsyB0Tra1wEN4KlrcI', card: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
     end
