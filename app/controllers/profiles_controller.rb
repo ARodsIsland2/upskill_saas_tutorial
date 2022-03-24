803b51567e08ab4f11374b7e5a19d3c1a42b0215
ARodsIsland2 <arods.island.2@gmail.com>
2022-03-24 18:22:09 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
   # GET request to /user/:user_id/profile/new
   def new
     # Render blank profile details form
+    @profile = Profile.new
   end
   
 end
